e9d84f5fe517b1def2fa1f9851e20dd0d5455469
Shi Bai <dairystone@gmail.com>
2016-05-19 14:29:51 -0700

    add more fields in service document

---
+++ src/main/java/services/UserAccountQueryService.java
@@ -0,0 +1,7 @@
+package services;
+
+/**
+ * Created by sbai on 5/19/16.
+ */
+public class UserAccountQueryService {
+}
--- src/main/java/services/UserAccountService.java
+++ src/main/java/services/UserAccountService.java
@@ -2,6 +2,8 @@ package services;
 
 import com.vmware.xenon.common.*;
 
+import java.util.*;
+
 /**
  * Created by sbai on 5/18/16.
  * This is the factory service used for managing user account.
@@ -18,6 +20,10 @@ public class UserAccountService extends StatefulService {
         public String userName;
         public String email;
         public String password;
+        @UsageOption(option = ServiceDocumentDescription.PropertyUsageOption.OPTIONAL)
+        public Set<String> tags = new HashSet<>();
+        @UsageOption(option = ServiceDocumentDescription.PropertyUsageOption.OPTIONAL)
+        public Map<String, String> keyValues = new HashMap<>();
     }
 
     public UserAccountService() {
@@ -42,6 +48,10 @@ public class UserAccountService extends StatefulService {
     @Override
     public void handlePost(Operation post) {
         System.out.println("In Post Handler");
+
+        addToState(post);
+
+        post.complete();
     }
 
     @Override
@@ -77,6 +87,18 @@ public class UserAccountService extends StatefulService {
         delete.complete();
     }
 
+    private void addToState(Operation add) {
+        UserAccountServiceState body = add.getBody(UserAccountServiceState.class);
+        UserAccountServiceState current = getState(add);
+
+        if (current != null) {
+            updateTags(body, current);
+            updateKeyValues(body, current);
+        }
+
+        add.setBody(current);
+    }
+
     private void updateState(Operation update, boolean force) {
         UserAccountServiceState body = update.getBody(UserAccountServiceState.class);
         UserAccountServiceState current = getState(update);
@@ -88,6 +110,22 @@ public class UserAccountService extends StatefulService {
         }
 
         update.setBody(current);
+    }
+
+    private void updateKeyValues(UserAccountServiceState body, UserAccountServiceState current) {
+        if (body != null && body.keyValues != null && !body.keyValues.isEmpty()) {
+            for (Map.Entry<String, String> entry : body.keyValues.entrySet()) {
+                current.keyValues.put(entry.getKey(), entry.getValue());
+            }
+        }
+    }
+
+    private void updateTags(UserAccountServiceState body, UserAccountServiceState current) {
+        if (body != null && body.tags != null && !body.tags.isEmpty()) {
+            for (String tag : body.tags) {
+                current.tags.add(tag);
+            }
+        }
     }
 
     private void updateUserName(UserAccountServiceState body, UserAccountServiceState
